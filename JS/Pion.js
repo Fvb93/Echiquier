0db9fa35c6ec32ca1e8bbb0f2bf641e69765c93d
Fabian Van Buggenhout <fabianvanb@proton.me>
2024-09-19 10:57:19 +0200

    Début de l'échiquier JS

---
+++ JS/Pion.js
@@ -0,0 +1,15 @@
+class Pion {
+    x = 0;
+    y = 0;
+    node;
+    color;
+
+    constructor(node, x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.node = node;
+        this.color = color;
+    }
+
+
+}
