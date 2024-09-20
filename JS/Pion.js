ad5d281edef61baa2133dc687cebc50a861941f6
haerphi <haerphi@gmail.com>
2024-09-20 08:58:51 +0200

    [Update] class Pion: Move + setEvent methods

--- JS/Pion.js
+++ JS/Pion.js
@@ -1,15 +1,31 @@
 class Pion {
-    x = 0;
-    y = 0;
-    node;
-    color;
+	table;
+	node;
+	x = 0;
+	y = 0;
+	color;
 
-    constructor(node, x, y, color) {
-        this.x = x;
-        this.y = y;
-        this.node = node;
-        this.color = color;
-    }
+	constructor(table, node, x, y, color) {
+		this.table = table;
+		this.node = node;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
 
+	setEvent() {
+		this.node.addEventListener("click", () => {
+			this.move();
+		});
+	}
 
+	getDirection() {
+		return this.color === "black" ? 1 : -1;
+	}
+
+	move() {
+		const movement = 1;
+		this.y += movement * this.getDirection();
+		this.table.rows[this.y].cells[this.x].appendChild(this.node);
+	}
 }
