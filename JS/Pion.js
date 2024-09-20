65d8f76a1e52f520d55c4ae0e6c20fc1cc4a5795
haerphi <haerphi@gmail.com>
2024-09-20 11:53:22 +0200

    [fix] class Pion: pionSelectionAbortController
    Quand on utilise AbortController, il faut en recréer un à chaque fois, car une fois qu'on utilise AbortController.abort(), ça rent AbortController inutilisable et il faut recréer une instance

--- JS/Pion.js
+++ JS/Pion.js
@@ -4,7 +4,7 @@ class Pion {
 	x = 0;
 	y = 0;
 	color;
-	pionSelectionAbortController = new AbortController();
+	pionSelectionAbortController;
 
 	possibleMovesDiv = [];
 
@@ -17,6 +17,7 @@ class Pion {
 	}
 
 	setPionSelectionEvent() {
+		this.pionSelectionAbortController = new AbortController();
 		this.node.addEventListener(
 			"click",
 			() => {
